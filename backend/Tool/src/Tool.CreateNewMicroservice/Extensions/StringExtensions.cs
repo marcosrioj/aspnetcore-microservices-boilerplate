8ab19cd71551832dabbdb2458cbf2ae5042633e2
Marcos Lima <marcoslima@marcoslima.com>
2020-04-11 12:04:32 -0700

    Added MicroserviceBaseProject and Initial create new microservice tool

--- backend/Tool/src/Tool.CreateNewMicroservice/Extensions/StringExtensions.cs
+++ backend/Tool/src/Tool.CreateNewMicroservice/Extensions/StringExtensions.cs
@@ -4,24 +4,13 @@ namespace Tool.CreateNewMicroservice.Extensions
 {
     internal static class StringExtensions
     {
-        public static string WrapWithTag(this string str, string tag)
+        public static string ToLowerFirstLetter(this string source)
         {
-            return $"<{tag}>{str}</{tag}>";
-        }
-
-        public static string ReplaceWithTag(this string sourceStr, string oldValue, string newValue, string tag)
-        {
-            return sourceStr.Replace(oldValue.WrapWithTag(tag), newValue.WrapWithTag(tag));
-        }
-
-        public static string GetDirectoryName(this string str)
-        {
-            return Path.GetDirectoryName(str);
-        }
-
-        public static string GetFileName(this string str)
-        {
-            return Path.GetFileName(str);
+            if (string.IsNullOrEmpty(source))
+                return string.Empty;
+            char[] letters = source.ToCharArray();
+            letters[0] = char.ToLower(letters[0]);
+            return new string(letters);
         }
     }
 }
--- backend/Tool/src/Tool.CreateNewMicroservice/Program.cs
+++ backend/Tool/src/Tool.CreateNewMicroservice/Program.cs
@@ -1,31 +1,203 @@
-﻿using System.Collections.Generic;
-using Tool.CreateNewMicroservice.Renamer;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Tool.CreateNewMicroservice.Extensions;
 
 namespace Tool.CreateNewMicroservice
 {
     class Program
     {
+        const string sourcePath = @"C:\work\marcoslima\aspnetcore-microservices-boilerplate\backend\MainProduct";
+        const string destinationPath = @"C:\work\marcoslima\aspnetcore-microservices-boilerplate\backend\MicroserviceBaseProject";
+
         static void Main(string[] args)
         {
-            var projectRenamer = new ProjectRenamer()
-            {
-                SolutionFullName = @"C:\work\marcoslima\aspnetcore-microservices-boilerplate\backend\All.sln",
-                ProjectFullName = @"C:\work\marcoslima\aspnetcore-microservices-boilerplate\backend\MainProduct\test\MainProduct.Tests\MainProduct.Tests.csproj",
-                ProjectName = "MainProduct",
-                ProjectUniqueName = @"MainProduct\MainProduct.csproj",
-                ProjectNameNew = "MicroserviceBaseProject",
-                SolutionProjects = new List<string>
-                {
-                    @"C:\work\marcoslima\aspnetcore-microservices-boilerplate\backend\MainProduct\src\MainProduct.Application\MainProduct.Application.csproj",
-                    @"C:\work\marcoslima\aspnetcore-microservices-boilerplate\backend\MainProduct\src\MainProduct.Core\MainProduct.Core.csproj",
-                    @"C:\work\marcoslima\aspnetcore-microservices-boilerplate\backend\MainProduct\src\MainProduct.EntityFrameworkCore\MainProduct.EntityFrameworkCore.csproj",
-                    @"C:\work\marcoslima\aspnetcore-microservices-boilerplate\backend\MainProduct\src\MainProduct.Migrator\MainProduct.Migrator.csproj",
-                    @"C:\work\marcoslima\aspnetcore-microservices-boilerplate\backend\MainProduct\src\MainProduct.Web.Core\MainProduct.Web.Core.csproj",
-                    @"C:\work\marcoslima\aspnetcore-microservices-boilerplate\backend\MainProduct\src\MainProduct.Web.Host\MainProduct.Web.Host.csproj",
-                }
-            };
+            //Console.Clear();
+            //Console.Write("Enter the name of the new microservice:");
+            //var solutionPath = GetSolutionPath();
+            //var projectName = Console.ReadLine();
 
-            projectRenamer.FullRename();
+            CopyFolder();
+            RenameFolder(destinationPath, "MainProduct", "MicroserviceBaseProject");
+            RenameFile(destinationPath, "MainProduct", "MicroserviceBaseProject");
+            ReplaceText(destinationPath, "MainProduct", "MicroserviceBaseProject");
+            AddSharedDetails("MicroserviceBaseProject");
+        }
+
+        static void CopyFolder()
+        {
+            foreach (string dirPath in Directory.GetDirectories(sourcePath, "*",
+                SearchOption.AllDirectories))
+                Directory.CreateDirectory(dirPath.Replace(sourcePath, destinationPath));
+
+            foreach (string newPath in Directory.GetFiles(sourcePath, "*.*",
+                SearchOption.AllDirectories))
+                File.Copy(newPath, newPath.Replace(sourcePath, destinationPath), true);
+        }
+
+
+        static void RenameFolder(string path, string source, string dest)
+        {
+            DirectoryInfo di = new DirectoryInfo(path);
+
+            foreach (DirectoryInfo si in di.GetDirectories("*", SearchOption.TopDirectoryOnly))
+            {
+                RenameFolder(si.Parent.FullName + @"\" + si.Name, source, dest);
+
+                string strFoldername = si.Name;
+                if (strFoldername.Contains(source))
+                {
+                    strFoldername = strFoldername.Replace(source, dest);
+                    string strFolderRoot = si.Parent.FullName + "\\" + strFoldername;
+
+                    si.MoveTo(strFolderRoot);
+                }
+            }
+        }
+
+        static void RenameFile(string path, string source, string dest)
+        {
+            DirectoryInfo di = new DirectoryInfo(path);
+
+            FileInfo[] files = di.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                File.Move(file.FullName, file.FullName.Replace(source, dest));
+            }
+
+            foreach (DirectoryInfo si in di.GetDirectories("*", SearchOption.TopDirectoryOnly))
+            {
+                RenameFile(si.Parent.FullName + @"\" + si.Name, source, dest);
+            }
+        }
+
+        static void ReplaceText(string path, string source, string dest)
+        {
+            DirectoryInfo di = new DirectoryInfo(path);
+
+            FileInfo[] files = di.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                StreamReader reader = new StreamReader(file.FullName, Encoding.Default);
+                string content = reader.ReadToEnd();
+                reader.Close();
+
+                content = Regex.Replace(content, source, dest);
+                content = Regex.Replace(content, source.ToLowerFirstLetter(), dest.ToLowerFirstLetter());
+
+                StreamWriter writer = new StreamWriter(file.FullName);
+                writer.Write(content);
+                writer.Close();
+            }
+
+            foreach (DirectoryInfo si in di.GetDirectories("*", SearchOption.TopDirectoryOnly))
+            {
+                ReplaceText(si.Parent.FullName + @"\" + si.Name, source, dest);
+            }
+        }
+
+        static void AddSharedDetails(string projectName)
+        {
+            DirectoryInfo di = new DirectoryInfo(GetSolutionPath());
+
+            var sharedCoreFolder = Path.Combine(di.FullName, $"Shared{Path.DirectorySeparatorChar}src{Path.DirectorySeparatorChar}Shared.Core");
+            if (!Directory.Exists(sharedCoreFolder))
+            {
+                throw new Exception("Could not find shared core project folder!");
+            }
+
+            var fileSharedConsts = Path.Combine(sharedCoreFolder, "SharedConsts.cs");
+            if (!File.Exists(fileSharedConsts))
+            {
+                throw new Exception("Could not find SharedConsts file!");
+            }
+
+            var fileAppConfigurations = Path.Combine(sharedCoreFolder, $"Configuration{Path.DirectorySeparatorChar}AppConfigurations.cs");
+            if (!File.Exists(fileAppConfigurations))
+            {
+                throw new Exception("Could not find AppConfigurations file!");
+            }
+
+            StringBuilder sbText = new StringBuilder();
+            using (var reader = new StreamReader(fileSharedConsts, Encoding.Default))
+            {
+                var line = string.Empty;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.Contains("SharedConnectionStringName"))
+                    {
+                        sbText.AppendLine(line);
+                        sbText.AppendLine(string.Empty);
+                        sbText.AppendLine($"        public const string {projectName}ConnectionStringName = \"{projectName}\";");
+                    }
+                    else
+                    {
+                        sbText.AppendLine(line);
+                    }
+                }
+            }
+
+            StreamWriter writer = new StreamWriter(fileSharedConsts);
+            writer.Write(sbText.ToString());
+            writer.Close();
+
+            sbText = new StringBuilder();
+            using (var reader = new StreamReader(fileAppConfigurations, Encoding.Default))
+            {
+                var line = string.Empty;
+                var firstExec = true;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.Contains("break;") && firstExec)
+                    {
+                        sbText.AppendLine(line);
+                        sbText.AppendLine($"                case \"{projectName}.EntityFrameworkCore.{projectName}DbContext\":");
+                        sbText.AppendLine($"                    connectionString = configuration.GetConnectionString(SharedConsts.{projectName}ConnectionStringName);");
+                        sbText.AppendLine($"                    break;");
+                        firstExec = false;
+                    }
+                    else
+                    {
+                        sbText.AppendLine(line);
+                    }
+                }
+            }
+
+            writer = new StreamWriter(fileAppConfigurations);
+            writer.Write(sbText.ToString());
+            writer.Close();
+        }
+
+
+        static bool FolderExist(string path)
+        {
+            DirectoryInfo di = new DirectoryInfo(path);
+            return di.Exists;
+        }
+
+        static string GetSolutionPath()
+        {
+            var programAssemblyDirectoryPath = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+            var di = new DirectoryInfo(programAssemblyDirectoryPath);
+            while (!DirectoryContains(di.FullName, "All.sln"))
+            {
+                if (di.Parent == null)
+                {
+                    throw new Exception("Could not find solution folder!");
+                }
+
+                di = di.Parent;
+            }
+
+            return di.FullName;
+        }
+
+        private static bool DirectoryContains(string directory, string fileName)
+        {
+            return Directory.GetFiles(directory).Any(filePath => string.Equals(Path.GetFileName(filePath), fileName));
         }
     }
 }
